1d7e16830d910b43e5765fcc90dfd6106b38e934
Muhammad Usman Liaqat <shannii54321@gmail.com>
2021-12-23 16:44:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signupnext2 from "./Components/Pages/Login Header/Signupnext2";
 import Signupnext3 from "./Components/Pages/Login Header/Signupnext3";
 import Signupnext4 from "./Components/Pages/Login Header/Signupnext4";
 import Singupnext1 from "./Components/Pages/Login Header/Singupnext1";
+import Welcome from "./Components/Pages/Login Header/Welome";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Singupnext1 /> */}
       {/* <Signupnext2 /> */}
       {/* <Signupnext3 /> */}
-      <Signupnext4 />
+      {/* <Signupnext4 /> */}
+      <Welcome />
     </div>
   );
 }
