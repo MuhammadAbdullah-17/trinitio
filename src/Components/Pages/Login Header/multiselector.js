f810527761c41fad9570e15ef8252e95287032e4
Muhammad Usman Liaqat <shannii54321@gmail.com>
2021-12-24 11:51:33 +0500

    Update multiselector.js

--- src/Components/Pages/Login Header/multiselector.js
+++ src/Components/Pages/Login Header/multiselector.js
@@ -4,159 +4,159 @@ import ClockImg from "../../../Assets/clock1.png";
 import Points from "../../../Assets/Group541.png";
 
 const Dayselector = () => {
-    // "#search".on("keyup", function () {
-    //   var pattern = $(this).val();
-    //   ".items-collection .items".hide();
-    //   ".items-collection .items"
-    //     .filter(function () {
-    //       return $(this).text().match(new RegExp(pattern, "i"));
-    //     })
-    //     .show();
-    // });
+    "#search".on("keyup", function () {
+      var pattern = $(this).val();
+      ".items-collection .items".hide();
+      ".items-collection .items"
+        .filter(function () {
+          return $(this).text().match(new RegExp(pattern, "i"));
+        })
+        .show();
+    });
   return (
-    <div class="container">
+    <div className="container">
       <h2>Multiple items selector with filter</h2>
-      <div class="row">
-        <div class="form-group">
-          <div class="items-collection">
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group bizmoduleselect">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+      <div className="row">
+        <div className="form-group">
+          <div className="items-collection">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group bizmoduleselect">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-car fa-2x"></span>
+                      <span className="fa fa-car fa-2x"></span>
                       <h5>car</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-truck fa-2x"></span>
+                      <span className="fa fa-truck fa-2x"></span>
                       <h5>truck</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-laptop fa-2x"></span>
+                      <span className="fa fa-laptop fa-2x"></span>
                       <h5>laptop</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-cube fa-2x"></span>
+                      <span className="fa fa-cube fa-2x"></span>
                       <h5>cube</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-lock fa-2x"></span>
+                      <span className="fa fa-lock fa-2x"></span>
                       <h5>secure</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-barcode fa-2x"></span>
+                      <span className="fa fa-barcode fa-2x"></span>
                       <h5>barcode</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-key fa-2x"></span>
+                      <span className="fa fa-key fa-2x"></span>
                       <h5>key</h5>
                     </div>
                   </label>
                 </div>
               </div>
             </div>
-            <div class="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
-              <div class="info-block block-info clearfix">
-                <div data-toggle="buttons" class="btn-group itemcontent">
-                  <label class="btn btn-default">
-                    <div class="itemcontent">
+            <div className="items col-xs-6 col-sm-3 col-md-3 col-lg-3">
+              <div className="info-block block-info clearfix">
+                <div data-toggle="buttons" className="btn-group itemcontent">
+                  <label className="btn btn-default">
+                    <div className="itemcontent">
                       <input
                         type="checkbox"
                         name="var_id[]"
                         autocomplete="off"
                         value=""
                       />
-                      <span class="fa fa-keyboard-o fa-2x"></span>
+                      <span className="fa fa-keyboard-o fa-2x"></span>
                       <h5>keyboard</h5>
                     </div>
                   </label>
@@ -166,14 +166,14 @@ const Dayselector = () => {
           </div>
         </div>
       </div>
-      <div class="row">
+      <div className="row">
         <br />
-        <div class="form-group">
-          <div class="col-sm-12 col-md-12 col-lg-12">
-            <i class="fa fa-search fa-2x pull-left searchicon"></i>
+        <div className="form-group">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <i className="fa fa-search fa-2x pull-left searchicon"></i>
             <input
               type="search"
-              class="form-control"
+              className="form-control"
               id="search"
               placeholder="Filter items..."
             />
