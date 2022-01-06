cc9d33b49158e83ea594ce5673f9ee137063c7cb
Muhammad Usman Liaqat <shannii54321@gmail.com>
2022-01-06 20:11:38 +0500

    Update Dashboard.js

--- src/Components/Pages/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "../../../App.css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +6,17 @@ import {
   Link,
   Routes,
 } from "react-router-dom";
+import MainDashboard from "./DashBoard Pages/NavbarndSidebar";
+import MainDashboard1 from "./DashBoard Pages/Main1";
+import Main1 from "./DashBoard Pages/Main1";
 
 function Dashboard() {
   return (
     <Router>
+      <MainDashboard />
       <Switch>
-        <Route exact path="/Dashboard" component={Dashboard} />
+        {/* <Route exact component={MainDashboard} /> */}
+        <Route exact path="/Dashboard" component={Main1} />
       </Switch>
     </Router>
   );
