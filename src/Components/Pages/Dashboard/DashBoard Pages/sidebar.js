57cee52310ee541d23fded31140fa4b69e74610d
Muhammad Usman Liaqat <shannii54321@gmail.com>
2022-01-19 17:35:02 +0500

    Update sidebar.js

--- src/Components/Pages/Dashboard/DashBoard Pages/sidebar.js
+++ src/Components/Pages/Dashboard/DashBoard Pages/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CDBSidebar,
   CDBSidebarContent,
@@ -10,28 +10,86 @@ import {
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const [show, setShow] = useState(false);
+  const [shown, setHide] = useState(false);
   return (
     <div className="side-right" style={{ overflow: "scroll initial" }}>
       <CDBSidebar textColor="#fff" backgroundColor="#333">
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
-            <NavLink exact to="/Dashboard" activeClassName="activeClicked">
+            <NavLink
+              exact
+              to="/Dashboard"
+              activeClassName="activeClicked"
+              style={{ textDecoration: "" }}
+            >
               <CDBSidebarMenuItem icon="home-lg">Dashboard</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/ManageUsers" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="user">Manage Users</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="calendar">
-                Appointment Scheduling
+            <NavLink exact to="/profile" activeClassName="activelist">
+              <CDBSidebarMenuItem
+                icon="calendar"
+                onClick={() => setShow(!show)}
+              >
+                Appointment Scheduling <i class="fas fa-chevron-right"></i>
               </CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="envelope">
-                Email Compaign
+            <CDBSidebarMenu>
+              {show ? (
+                <div className="">
+                  <NavLink exact to="/profile" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem>
+                      <i class="fas fa-chevron-right txt-sdbr mx-2"></i>
+                      Dashboard
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/u" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem>
+                      <i class="fas fa-chevron-right txt-sdbr mx-2"></i>
+                      Integrations
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/y" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem>
+                      <i class="fas fa-chevron-right txt-sdbr mx-2"></i>
+                      Connect your Calendar
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                </div>
+              ) : null}
+            </CDBSidebarMenu>
+            <NavLink exact to="/Email/Dashboard" activeClassName="activelist">
+              <CDBSidebarMenuItem
+                icon="envelope"
+                onClick={() => setHide(!shown)}
+              >
+                Email Compaign <i class="fas fa-chevron-right ml-5 pl-3"></i>
               </CDBSidebarMenuItem>
             </NavLink>
-
+            <CDBSidebarMenu>
+              {shown ? (
+                <div className="">
+                  <NavLink
+                    exact
+                    to="/Email/Dashboard"
+                    activeClassName="activeClicked"
+                  >
+                    <CDBSidebarMenuItem>
+                      <i class="fas fa-chevron-right txt-sdbr mx-2"></i>
+                      Dashboard
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/u" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem>
+                      <i class="fas fa-chevron-right txt-sdbr mx-2"></i>
+                      Added ‘From’ Email Accounts
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                </div>
+              ) : null}
+            </CDBSidebarMenu>
             <NavLink
               exact
               to="/hero404"
