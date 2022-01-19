e0e632a077a4c56596a52e5b2f34160252d702d3
Muhammad Usman Liaqat <shannii54321@gmail.com>
2022-01-20 00:44:06 +0500

    Update EmailRecipients.js

--- src/Components/Pages/Dashboard/DashBoard Pages/EmailRecipients.js
+++ src/Components/Pages/Dashboard/DashBoard Pages/EmailRecipients.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../../../../Wclasses.css";
 import "../../../../Hclasses.css";
+import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
 // import { TextField } from "@material-ui/core";s
 
 const EmailRecipients = () => {
@@ -58,20 +60,148 @@ const EmailRecipients = () => {
               </div>
             </div>
           </div>
-          <div className="w-70 h-30 mx-3 brdr-r-2 bg-greyM cdtl mt-3">
-            <div className="d-flex">
-              <div className="col text-center">Status</div>
-              <div className="col text-center">#</div>
-              <div className="col-2 text-left">First Name</div>
-              <div className="col-2 text-left">Last Name</div>
-              <div className="col-3 text-left">Email</div>
-              <div className="col-2 text-left">Company</div>
-              <div className="col-4 text-left">Linkedin Profile</div>
-              <div className="col-1 text-left">Skype</div>
-              <div className="col-2 text-left">Merge Tag 1</div>
-              <div className="col-2 text-left">Merge Tag 2</div>
+          <div className="w-70 h-30 cdtl mx-3 brdr-r-2 mt-4">
+            <Table responsive className="w-98vw">
+              <thead>
+                <tr className="bg-greyM">
+                  <th className="text-center">Status</th>
+                  <th className="text-center">#</th>
+                  <th className="text-left">First Name</th>
+                  <th className="text-left">Last Name</th>
+                  <th className="text-left">Email</th>
+                  <th className="text-left">Company</th>
+                  <th className="text-left">Linkedin Profile</th>
+                  <th className="text-left">Skype</th>
+                  <th className="text-left">Merge Tag 1</th>
+                  <th className="text-left">Merge Tag 2</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+                <tr>
+                  <td className="text-center ifobg">
+                    <i class="fas fa-info-circle"></i>
+                  </td>
+                  <td className="text-center">#</td>
+                  <td>Piyush</td>
+                  <td>Patel</td>
+                  <td>piyush@saleshandy.com</td>
+                  <td>SalesHandy Inc.</td>
+                  <td>https://www.linkedin.com/in/piyushnp</td>
+                  <td>piyushnp88</td>
+                  <td>custom message 1</td>
+                  <td>custom message 1</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <div className="d-flex mx-auto mt-3">
+            <div className="mx-3 w-18vw">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="CC"
+                className="w-100 py-2 px-3 brdr-r-2 outline-0 brdr-grey"
+              />
+            </div>
+            <div className="w-18vw">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="BCC"
+                className="w-100 py-2 px-3 brdr-r-2 outline-0 brdr-grey"
+              />
             </div>
           </div>
+          <div className="col-7 mt-2 font-15 mx-1">
+            We recommend that while sending emails from Gmail account, please Do
+            NOT specify the same account in CC and/or BCC for accurate results
+            in replay tracking.
+          </div>
+          <div className="col-7 mt-3">
+            <div className="row">
+              <div className="col mx-1">
+                  <h5 className="mt-2">From Email</h5>
+              </div>
+              <div className="col text-right">
+                  <button className="border-0 brdr-r-20 bg-green text-white px-4 py-2 mx-3">
+                    + Add Email Campaign
+                  </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-7 mt-3 brdr-r-2 brdr-grey mx-3"></div>
         </div>
       </div>
     </div>
